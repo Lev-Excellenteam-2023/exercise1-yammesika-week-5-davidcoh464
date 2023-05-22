479ea31b6b1400cf35968c4d7008e2e2f942adfb
David Cohen <deiv464@gmail.com>
2023-05-22 11:32:16 +0300

    add documentation

--- 5.1.vinaigrette.py
+++ 5.1.vinaigrette.py
@@ -26,19 +26,38 @@ def string_to_date(str_date: str) -> datetime:
         raise ValueError("Invalid date format: YYYY-MM-DD")
 
 
-def vinaigrette(start_date, end_date):
+def vinaigrette(start_date: str, end_date: str) -> str:
+    """
+    Generates a random date within the specified date range and checks if it falls on a Monday.
+
+    Args:
+        start_date (str): The start date in the format 'YYYY-MM-DD'.
+        end_date (str): The end date in the format 'YYYY-MM-DD'.
+
+    Returns:
+        str: A string representation of the randomly generated date in the format 'YYYY-MM-DD'.
+
+    Raises:
+        ValueError: If the start date or end date not in format of 'YYYY-MM-DD'.
+        ValueError: If the start date is greater than the end date.
+
+    Examples:
+        >>> vinaigrette('2023-05-17', '2023-05-31')
+        '2023-05-25'
+
+    """
     start_date = string_to_date(start_date)
     end_date = string_to_date(end_date)
-    
+
     if start_date > end_date:
         raise ValueError("Start date cannot be greater than end date")
-        
+
     random_date = start_date + (end_date - start_date) * random.random()
     if random_date.weekday() == 0:
-        print("אין לי ויניגרט!")
+        print("Don't have vinaigrette!")
 
     return str(random_date.date())
 
 
 if __name__ == "__main__":
-    print(vinaigrette('1912-06-23', '1954-06-07'))
+    print(vinaigrette('1950-06-23', '2000-06-07'))
