172f6db597ae843944938fdfc7e61dc3c5189ae1
David Cohen <deiv464@gmail.com>
2023-05-22 09:49:49 +0300

    Review change

--- 5.2.join.py
+++ 5.2.join.py
@@ -1,18 +1,44 @@
-def join(*params, sep = '-'):
+def join(*params, separator: str = '-') -> list:
+    """
+    Concatenates multiple iterables with a specified separator.
+
+    Args:
+        *params: Variable-length arguments representing iterables.
+        separator (str, optional): Separator to be inserted between concatenated elements. Defaults to '-'.
+
+    Returns:
+        list: A list containing the concatenated elements from the input iterables.
+
+    Raises:
+        TypeError: If any of the input arguments is not iterable.
+
+    Examples:
+        >>> join([1, 2, 3], [4, 5, 6])
+        [1, 2, 3, '-', 4, 5, 6]
+
+        >>> join(['Hello', 'World'], ['Open', 'AI'], separator=' ')
+        ['Hello', 'World', ' ', 'Open', 'AI']
+
+    """
     if not params:
         return None
-    L = []
-    for i in range(len(params)):
-        try:
-            iter(params[i])
-        except:
-            raise TypeError(params[i], 'is not iterable')
-        for j in range(len(params[i])):
-            L.append(params[i][j])
-        L.append(sep)
-    del L[-1]
-    return L
-print(join([1, 2], [8], [9, 5, 6], sep = '@'))
-print(join([1, 2], [8], [9, 5, 6]))
-print(join([1]))
-print(join())
+
+    result_list = []
+    for param in params:
+        if not isinstance(param, (list, tuple)):
+            raise TypeError(f"{param} is not iterable")
+
+        result_list.extend(param)
+        result_list.append(separator)
+
+    if result_list:
+        del result_list[-1]
+
+    return result_list
+
+
+if __name__ == "__main__":
+    print(join([1, 2], [8], [9, 5, 6], separator='@'))
+    print(join([1, 2], [8], [9, 5, 6]))
+    print(join([1], 6, 7))
+    print(join())
